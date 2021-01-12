332374dfd965b357485d2334b2513a265326339c
HiroyaKadowaki <168newdis@gmail.com>
2021-01-12 11:27:53 +0900

    一時コメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
-  pravate
-  def item_params
-    params.require(:item).permit(:name, :number, :comment).merge(user_id: current_user.id)
-  end
+  # pravate
+  # def item_params
+  #   params.require(:item).permit(:name, :number, :comment).merge(user_id: current_user.id)
+  # end
 end
