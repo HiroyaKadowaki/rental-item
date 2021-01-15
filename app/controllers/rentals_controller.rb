2c41001111ff9804948bd87b2d26f6a9b59e885e
HiroyaKadowaki <168newdis@gmail.com>
2021-01-15 15:35:19 +0900

    貸し出し返却機能の追加

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,24 +1,28 @@
 class RentalsController < ApplicationController
   before_action :move_to_index
+  before_action :set_rental, only: [:edit, :destroy]
 
   def index
     @rentals = Rental.new
   end
 
   def create
-    @rental = Rental.new
+    @rental = Rental.new(rental_params)
     if @rental.save
       redirect_to root_path
     else
-      render :new
+      render action: :index
     end
   end
 
+  def edit
+  end
+
   def destroy
-    # if @rental.user_id == current_user.id
-    #   @rental.destroy
-    # end
-    # redirect_to root_path
+     if current_user.id == @rental.user_id
+       @rental.destroy
+     end
+     redirect_to root_path
   end
 
   private
@@ -27,4 +31,13 @@ class RentalsController < ApplicationController
       redirect_to new_user_session_path action: :index
     end
   end
+
+  def rental_params
+    params.permit(:rental_day).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_rental
+    @rental = Rental.find(params[:id])
+  end
+
 end
--- db/migrate/20210107065307_create_rentals.rb
+++ db/migrate/20210107065307_create_rentals.rb
@@ -1,7 +1,7 @@
 class CreateRentals < ActiveRecord::Migration[6.0]
   def change
     create_table :rentals do |t|
-      t.date :rental_day
+      t.string :rental_day
       t.references :user, foreign_key: true
       t.references :item, foreign_key: true
       t.timestamps
